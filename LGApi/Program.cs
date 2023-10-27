5f20719392bf35c36d38c20dff0fe52702bd79da
Ronye Rocha <ronye.rocha@gmail.com>
2023-10-26 23:22:02 -0300

    spacing

--- LGApi/Program.cs
+++ LGApi/Program.cs
@@ -8,42 +8,42 @@ using LGApi.Repositories;
 using LGApi.Validators;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers().AddJsonOptions(opt =>
-{
-    opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-});
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddCors();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-// builder.Services.AddDbContext<LgApiContext>(opt =>
-// {
-//     opt.UseNpgsql(builder.Configuration.GetConnectionString("LgApiConnectionString"));
-//     opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-// });
+    builder.Services.AddControllers().AddJsonOptions(opt =>
+    {
+        opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddCors();
+    builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    // builder.Services.AddDbContext<LgApiContext>(opt =>
+    // {
+    //     opt.UseNpgsql(builder.Configuration.GetConnectionString("LgApiConnectionString"));
+    //     opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    // });
 
-builder.Services.AddDbContext<LgApiContext>(opt =>
-{
-    opt.UseNpgsql(Environment.GetEnvironmentVariable("LgApiConnectionString"));
-    opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-});
+    builder.Services.AddDbContext<LgApiContext>(opt =>
+    {
+        opt.UseNpgsql(Environment.GetEnvironmentVariable("LgApiConnectionString"));
+        opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    });
 
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddScoped<LgApiContext>();
-builder.Services.AddScoped<IContaRepository, ContaRepository>();
-builder.Services.AddFluentValidationAutoValidation();
-builder.Services.AddValidatorsFromAssemblyContaining<ContaValidator>();
+    builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    builder.Services.AddScoped<LgApiContext>();
+    builder.Services.AddScoped<IContaRepository, ContaRepository>();
+    builder.Services.AddFluentValidationAutoValidation();
+    builder.Services.AddValidatorsFromAssemblyContaining<ContaValidator>();
 
-var app = builder.Build();
+    var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseMiddleware<RequestSerilogMiddleware>();
-app.UseMiddleware<ErrorHandlingMiddleware>();
-app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-app.UseAuthorization();
-app.MapControllers();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseMiddleware<RequestSerilogMiddleware>();
+    app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+    app.UseAuthorization();
+    app.MapControllers();
 
-app.Run();
+    app.Run();
